77c514c1787f0c990425cfc04b4d9f200489a59f
Jiby Krishna kg <jibykrishnakg@Jibys-MacBook-Air.local>
2023-02-01 12:42:07 -0500

    Retrieve pateint entry from database based on the record

--- database_new.py
+++ database_new.py
@@ -7,6 +7,21 @@ def main_driver():
     db.append(create_patient_entry("Arun John",1,28))
     db.append(create_patient_entry("Jiby Krishna",2,28))
     print(db)
+    #print(db[1] [2])
+    print("Get patient Arun")
+    mrn_to_find=4
+    found_patient=get_patient_entry(db,mrn_to_find)
+  #  if found_patient is False:
+    if found_patient == False:
+        print("Patient mrn {} is not found".format(mrn_to_find))
+    else:
+        print(found_patient)
+
+def get_patient_entry(db,mrn_to_find):
+    for patient in db:
+        if patient[1]==mrn_to_find:
+            return patient
+    return False 
 
 if __name__ == "__main__":
     main_driver()
