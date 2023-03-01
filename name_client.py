e6da0d9cb6d2b23fb888d072f97c101604511f4e
Jiby KG <jibykrishnakg@gmail.com>
2023-03-01 13:21:10 -0500

    client code are editied

--- name_client.py
+++ name_client.py
@@ -5,7 +5,9 @@ out_data = {"name": "Jiby Krishna",
             "e-mail": "jiby.kg@duke.edu" }
 
 r = requests.post("http://vcm-21170.vm.duke.edu:5000/student", json = out_data)
-
 print(r.status_code)
+print(r.text)
 
+r = requests.post("http://vcm-21170.vm.duke.edu:5000/list", json = out_data)
+print(r.status_code)
 print(r.text)
